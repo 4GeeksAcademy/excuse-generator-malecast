be7cd12fc61d6d4f2f24082fbc9e02a8406211cd
malecast <malecast.12@gmail.com>
2025-11-26 21:08:12 +0000

    Código generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    generateExcuse();
+};
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+function generateExcuse() {
+            let excuseWho = who[Math.floor(Math.random() * who.length)];
+            let excuseAction = action[Math.floor(Math.random() * action.length)];
+            let excuseWhat = what[Math.floor(Math.random() * what.length)]; 
+            let excuseWhen = when[Math.floor(Math.random() * when.length)];
+
+            let finalExcuse = `${excuseWho} ${excuseAction} ${excuseWhat} ${excuseWhen}`;
+            document.querySelector("#excuse").innerHTML = finalExcuse;
 };
